452e96f90144738d3b37e9a856495c1dc7e87908
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2023-08-29 16:01:04 -0300

    iniciando o javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+var num = document.getElementsByNameName('span')
+var caixa = document.getElementsByClassName('principal')
+
+
+function numeros() {
+    if (num < 1) {
+        alert('Não selecionado')
+    } 
+}
