f1febd0df960601ed5b1ee5ffcd9a42e4e7b4f66
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2023-09-05 19:06:58 -0300

    quebrando a cabeça

--- html/js/script.js
+++ html/js/script.js
@@ -1,15 +1,5 @@
-var span = document.querySelector(".sp")
-var res = document.getElementsByClassName("resultado")
-
-function submit() {
-    var bola1 = document.getElementsByClassName("fiel1")
-    if (bola1) {  
-        bola1 = Number(bola1.value)
-        bola1 = 1  
-        res.innerHTML = `You selected ${bola1} out of 5 `
-    }
-    
-}
+const buttons = document.querySelectorAll(".numeros .button")
+const submit = document.querySelector(".enviar")
 
 
     
