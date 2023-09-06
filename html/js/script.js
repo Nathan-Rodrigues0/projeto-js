66a5c72323fa5af045e7479dece641cb8a07c061
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2023-09-06 19:09:22 -0300

    tentando e estudando

--- html/js/script.js
+++ html/js/script.js
@@ -1,44 +1,19 @@
-/*const buttons = document.querySelectorAll(".numeros .button")
-const submit = document.querySelector(".enviar")
-const res = document.querySelector(".resultado")
-const valor = document.querySelector(".sp")
+const button = document.querySelectorAll(".number-button");
+const submit = document.querySelector(".submit");
+const result = document.querySelector(".result");
+const main = document.querySelector(".main");
+const number = document.querySelector(".number");
 
-buttons.forEach(button) => {
-    button.addEventListener("click", (event) => {
-        event.preventDefault();
-        buttons.forEach((otherButton) => {
-            otherButton.classList.remove("active");
-        });
-        button.classList.add("active")
-        submit.disabled = false;
-    });
-}*/
-const ratingButtons = document.querySelectorAll(".numeros .button");
-const submitButton = document.querySelector(".button-submit");
-const thankYouContainer = document.querySelector(".resultado");
-const ratingValue = document.querySelector(".sp");
-
-ratingButtons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    event.preventDefault();
-    ratingButtons.forEach((otherButton) => {
-      otherButton.classList.remove("active");
-    });
-    button.classList.add("active");
-    submitButton.disabled = false;
-  });
-});
-
-submitButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  const activeButton = document.querySelector(
-    ".numeros .button.active"
-  );
-  if (activeButton) {
-    activeButton.disabled = true;
-    document.querySelector(".principal").style.display = "none";
-    thankYouContainer.style.display = "block";
-    ratingValue.textContent = activeButton.dataset.rating;
+submit.addEventListener("click", function () {
+  if (number.innerHTML) {
+    main.classList.add("hidden");
+    result.classList.remove("hidden");
   }
 });
-    
+
+button.forEach(number) => {
+    number.addEventListener("click", function (e) {
+    const num = e.target.dataset.ket;
+    number.innerHTML = num;
+  });
+};
