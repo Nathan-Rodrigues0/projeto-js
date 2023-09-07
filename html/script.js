9df718237e2ae5d1bcf04a44102ad949c419a779
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2023-09-07 14:32:44 -0300

    javascript em funcionamento

--- html/script.js
+++ html/script.js
@@ -2,18 +2,21 @@ const button = document.querySelectorAll(".number-button");
 const submit = document.querySelector(".submit");
 const result = document.querySelector(".result");
 const main = document.querySelector(".main");
-const number = document.querySelector(".number");
+const num = document.querySelector(".sp-number");
 
 submit.addEventListener("click", function () {
-  if (number.innerHTML) {
+  winow.location.href = "index-2.html";  
+  if (num.innerHTML) {
     main.classList.add("hidden");
     result.classList.remove("hidden");
   }
 });
 
-button.forEach(number) => {
+button.forEach((number) =>  {
     number.addEventListener("click", function (e) {
-    const num = e.target.dataset.ket;
-    number.innerHTML = num;
+    const n = e.target.dataset.key;
+    num.innerHTML = n;
   });
-};
+});
+
+
